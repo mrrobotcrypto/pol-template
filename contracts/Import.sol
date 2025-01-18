591057a8a52432d3e94c9b5beaa694651607c357
SovaSniper <SovaSniper@outlook.com>
2025-01-18 12:16:08 +1100

    update assets

--- contracts/Import.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "./01_deploy_your_first_token/SimpleContract.sol";
-import "./02_tokens_using_openzeppelin/SampleERCToken.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Import {}
