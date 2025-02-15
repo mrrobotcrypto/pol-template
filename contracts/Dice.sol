49c131332fcdd428df72a11fca160bf0033b351f
mrrobotcrypto <hsn_dyn721@hotmail.com>
2025-02-15 11:21:45 +0300

    Dice.sol

--- contracts/Dice.sol
+++ contracts/Dice.sol
@@ -10,13 +10,20 @@ contract DiceGame {
     IVRRF public immutable vrrf;
     event RollEvent(uint256 timestamp, uint256 n, uint256 value);
 
-    // Sabit VRRF adresini constructor'a ekleyelim
-    constructor() {
-        vrrf = IVRRF(0xDb14c007634F6589Fb542F64199821c3308A9d92);
+    constructor(address _vrrf) {
+        vrrf = IVRRF(_vrrf);
     }
 
     function roll() public returns (uint8) {
         bytes32 salt = blockhash(block.number - 1);
+        uint256 n = uint256(vrrf.random(salt));
+        uint8 value = uint8((n % 6) + 1);
+
+        emit RollEvent(block.timestamp, n, value);
+        return value;
+    }
+
+    function rollWithSalt(bytes32 salt) public returns (uint8) {
         uint256 n = uint256(vrrf.random(salt));
         uint8 value = uint8((n % 6) + 1);
 
